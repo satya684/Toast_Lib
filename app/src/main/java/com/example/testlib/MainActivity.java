266c849b6028b84b7fd01ece58d93e773eba7f39
Satya <satyap0899@gmail.com>
2022-03-18 18:14:23 +0530

    first_lib

---
+++ app/src/main/java/com/example/testlib/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.testlib;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.toasty.Toaster;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toaster.simpleToast(MainActivity.this,"Check");
+    }
+}
